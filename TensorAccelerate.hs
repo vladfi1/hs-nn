648414aab2353aef2be4f213852357dbf38925b6
Vlad Firoiu <vladfi2@gmail.com>
2016-01-25 20:11:40 -0500

    Num instance for ATensor.

--- TensorAccelerate.hs
+++ TensorAccelerate.hs
@@ -7,38 +7,47 @@
   FlexibleContexts,
   RankNTypes,
   InstanceSigs,
-  ScopedTypeVariables
+  ScopedTypeVariables,
+  UndecidableInstances
   #-}
 
 module TensorAccelerate where
 
 import Data.Array.Accelerate as A
-import Data.Singletons
-import Generics.SOP.Constraint (And)
+import Data.Singletons.Prelude hiding ((:.))
+import Generics.SOP.Constraint as C
 import GenericTensor
 
 type family Dims2Shape (dims :: [k]) :: * where
   Dims2Shape '[] = Z
   Dims2Shape (n ': dims) = Dims2Shape dims :. Int
 
+-- a bit of redundancy in these types/proofs, but this is the cleanest thing I could do
+-- the point of all this is to cache Shape (Dims2Shape dims) proofs
+-- we could of course reconstruct them from SingI dims as needed
+-- but this saves us some pain in the subsequent ATensor instances
 data SDims dims where
   DimsZ :: SDims '[]
-  DimsS :: SDimsI dims => SDims (a ': dims)
+  DimsS :: SDimsI dims => Sing n -> SDims dims -> SDims (n ': dims)
 
-class (Shape (Dims2Shape dims)) => SDimsI dims where
+-- we need to use All SingI dims instead of just SingI dims to help the typechecker
+-- the issue is that ghc doesn't work backwards from instance declarations
+-- that is, given instance A x => B x and B x, ghc can't deduce A x, even if B x has no overlaps
+-- in contrast, the All constraint former is a just a type family and ghc will happily expand it
+class (C.All SingI dims, IntegralL dims, Shape (Dims2Shape dims)) => SDimsI dims where
   sDims :: SDims dims
 
-instance SDimsI '[] where
+instance (IntegralK ('KProxy :: KProxy k)) => SDimsI ('[] :: [k]) where
   sDims = DimsZ
 
-instance SDimsI dims => SDimsI (a ': dims) where
-  sDims = DimsS
+instance (SingI n, SDimsI dims) => SDimsI (n ': dims) where
+  sDims = DimsS sing sDims
 
 data ATensor (dims :: [k]) a where
   ATensor :: (SDimsI dims, Elt a) => Acc (Array (Dims2Shape dims) a) -> ATensor dims a
-  
+
 instance Tensor ATensor where
-  type N ATensor = And Elt IsNum
+  type N ATensor = C.And Elt IsNum
   
   --asCol :: N t a => t '[n] a -> t '[n, FromInteger 1] a
   --asRow :: N t a => t '[n] a -> t '[FromInteger 1, n] a
@@ -70,4 +79,17 @@ instance Tensor ATensor where
   -- FIXME: constant has bad performance?
   oneHot :: forall n a. (N ATensor a, SingI n, IntegralN n) => Int -> ATensor '[n] a
   oneHot i = ATensor $ generate (constant $ Z :. natVal' (sing :: Sing n)) (\j -> cond (constant i ==* unindex1 j) 1 0)
+
+dims2Shape :: (IntegralL dims) => SDims dims -> Dims2Shape dims
+dims2Shape DimsZ = Z
+dims2Shape (DimsS n l) = dims2Shape l :. natVal' n
+
+instance (SDimsI dims, IntegralL dims, Elt a, IsNum a) => Num (ATensor dims a) where
+  ATensor a + ATensor b = ATensor $ A.zipWith (+) a b
+  ATensor a - ATensor b = ATensor $ A.zipWith (-) a b
+  ATensor a * ATensor b = ATensor $ A.zipWith (*) a b
+  negate (ATensor a) = ATensor (A.map negate a)
+  abs (ATensor a) = ATensor (A.map abs a)
+  signum (ATensor a) = ATensor (A.map signum a)
+  fromInteger n = ATensor $ fill (constant $ dims2Shape (sDims :: SDims dims)) (constant $ fromInteger n)
 
